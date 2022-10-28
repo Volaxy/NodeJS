c5b70738f9b973b1c6d7d3d40439023b3c78b874
Gustavo Martins Pereira <guga.PRO.00@hotmail.com>
2022-10-28 14:14:45 -0300

    ADD - Response Objects with Express

--- modules/express.js
+++ modules/express.js
@@ -7,6 +7,21 @@ app.get("/home", (req, res) => {
     res.status(200).send("<h1>Hello world!</h1>");
 });
 
+app.get("/users", (req, res) => {
+    const users = [
+        {
+            name: "John Doe",
+            email: "john@doe.com",
+        },
+        {
+            name: "Jone Doe",
+            email: "jone@doe.com",
+        }
+    ];
+
+    res.status(200).json(users);
+});
+
 const port = 8080;
 
 app.listen(port, () => console.log(`Running with Express in the port http://localhost:${port}`));
