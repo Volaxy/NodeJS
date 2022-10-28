6ee9fd25a82c65483e8c61066812189a0ff330b5
Gustavo Martins Pereira <guga.PRO.00@hotmail.com>
2022-10-28 13:54:34 -0300

    ADD - End point Created with Basic Response

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-require("./modules/fs")
+const http = require("http");
+
+const port = 8080;
+const server = http.createServer((req, res) => {
+    if(req.url === "/home") {
+        // Assign the HTTP Code in the response
+        // Specify the type of the file
+        res.writeHead(200, { "Content-Type": "text/html" });
+
+        // The content that will be returned after the request is requested
+        res.end("<h1>Home Page</h1>");
+    }
+});
+
+server.listen(port, () => console.log(`Listening in the port localhost:${port}`));
