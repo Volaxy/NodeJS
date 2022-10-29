eb17f8090a732d8fc650579c2d61cadc3910aabf
Gustavo Martins Pereira <guga.PRO.00@hotmail.com>
2022-10-29 13:54:42 -0300

    ADD - DELETE Method - User By Id

--- modules/express.js
+++ modules/express.js
@@ -60,6 +60,19 @@ app.patch("/users/:id", async (req, res) => {
     }
 });
 
+// DELETE
+// User By Id
+app.delete("/users/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
+        const user = await UserModel.findByIdAndRemove(id);
+
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
 const port = 8080;
 
 app.listen(port, () => console.log(`Running with Express in the port http://localhost:${port}`));
