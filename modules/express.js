6819d23f4ae23ef889c5ede20db3f71626ffda03
Gustavo Martins Pereira <guga.PRO.00@hotmail.com>
2022-10-29 13:50:14 -0300

    ADD - PATCH Method - Change Attribute User By Id

--- modules/express.js
+++ modules/express.js
@@ -47,6 +47,19 @@ app.post("/users", async (req, res) => {
     }
 });
 
+// PATCH
+// Attribute User
+app.patch("/users/:id", async (req, res) => {
+    try {
+        const id = req.params.id;
+        const user = await UserModel.findByIdAndUpdate(id, req.body, { new: true });
+
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+});
+
 const port = 8080;
 
 app.listen(port, () => console.log(`Running with Express in the port http://localhost:${port}`));
