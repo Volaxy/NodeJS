34a91c6de42427e1115954ac0d15761ac3bca09a
Gustavo Martins Pereira <guga.pro.00@hotmail.com>
2022-11-08 19:03:00 -0300

    FIX - Environment Variable USER with SO

--- src/database/connectionPostgres.js
+++ src/database/connectionPostgres.js
@@ -4,7 +4,7 @@ const pgp = require("pg-promise")();
 dotenv.config();
 
 const db = pgp({
-    user: process.env.USER,
+    user: process.env.USERNAME_DB,
     password: process.env.PASSWORD,
     host: process.env.HOST,
     port: process.env.PORT,
